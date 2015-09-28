1a3b661c2652d826579f918d0365fb45483b28df
Melanie <mlnxromano@gmail.com>
2015-09-28 17:09:09 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/* 
+ _____   _           _         _                        _                  
+|_   _| | |         | |       | |                      | |                 
+  | |   | |__   __ _| |_ ___  | |_ ___  _ __ ___   __ _| |_ ___   ___  ___ 
+  | |   | '_ \ / _` | __/ _ \ | __/ _ \| '_ ` _ \ / _` | __/ _ \ / _ \/ __|
+ _| |_  | | | | (_| | ||  __/ | || (_) | | | | | | (_| | || (_) |  __/\__ \
+ \___/  |_| |_|\__,_|\__\___|  \__\___/|_| |_| |_|\__,_|\__\___/ \___||___/
+
+Oh nice, welcome to the js file of dreams.
+Enjoy responsibly!
+@ihatetomatoes
+
+*/
+
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('h1').css('color','#222222');
+	}, 3000);
+	
+
+	$('.mdl-navigation__link').click(function(){
+		var newActiveID = $(this).data('href');
+		$('.mdl-layout__tab.is-active, .mdl-layout__tab-panel.is-active').removeClass('is-active');
+		$(newActiveID + ', .mdl-layout__tab[href=' + newActiveID + ']').addClass('is-active');
+	})
+});
